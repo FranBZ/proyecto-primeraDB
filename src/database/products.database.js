dc71a3af97f1614c9186d5f7dd221977edab58a7
FranBZ <franciscobenitezjan@gmail.com>
2022-09-14 20:25:33 -0300

    proyecto funcional

---
+++ src/database/products.database.js
@@ -0,0 +1,10 @@
+/* Conneccion a base de datos mysql */
+export const productsOptions = {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'coderhouse'
+    }
+}
